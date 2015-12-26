6fbd07f8800a3dcc96946b14f273fefc02ecd67a
Kalinovsky Vitaliy <wolfrnd@ya.ru>
2015-12-26 14:23:38 +0500

    test

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
 /*app.get('*', function(req, res) {
   res.end('Test by Forest');
