f4390ccd8cabecf38ed5e893ed9ff26ec715b2ad
Kalinovsky Vitaliy <wolfrnd@ya.ru>
2015-12-26 14:21:31 +0500

    test

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static('public'));
 
-app.get('*', function(req, res) {
+/*app.get('*', function(req, res) {
   res.end('Test by Forest');
-});
+});*/
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
